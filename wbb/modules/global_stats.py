192154af6e07dfdef36b0a3724146bfcb7a72c92
Nethmal Prabashwara <96367042+neth1841@users.noreply.github.com>
2022-03-06 10:21:33 +0530

    Update global_stats.py

--- wbb/modules/global_stats.py
+++ wbb/modules/global_stats.py
@@ -102,8 +102,8 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
-    rurl = "https://github.com/thehamkercat/williambutcherbot"
+    url = "https://api.github.com/repos/neth1841/contributors"
+    rurl = "https://github.com/neth1841"
     developers = await get(url)
     commits = 0
     for developer in developers:
